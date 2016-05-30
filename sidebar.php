d0022d69ad98edf20eafe4d696cb369d7eb10c3f
jlsodai <jlsodai@gmail.com>
2016-05-30 14:45:28 +0000

    remove sidebar content into sidebar.php to be included in pages

---
+++ sidebar.php
@@ -0,0 +1,189 @@
+						<div class="sidebar-widgets-wrap">
+
+							<div class="widget widget_links clearfix">
+
+								<h4>Shop Categories</h4>
+								<ul>
+									<li><a href="#">Shirts</a></li>
+									<li><a href="#">Pants</a></li>
+									<li><a href="#">Tshirts</a></li>
+									<li><a href="#">Sunglasses</a></li>
+									<li><a href="#">Shoes</a></li>
+									<li><a href="#">Bags</a></li>
+									<li><a href="#">Watches</a></li>
+								</ul>
+
+							</div>
+
+							<div class="widget clearfix">
+
+								<h4>Recent Items</h4>
+								<div id="post-list-footer">
+
+									<div class="spost clearfix">
+										<div class="entry-image">
+											<a href="#"><img src="images/shop/small/1.jpg" alt="Image"></a>
+										</div>
+										<div class="entry-c">
+											<div class="entry-title">
+												<h4><a href="#">Blue Round-Neck Tshirt</a></h4>
+											</div>
+											<ul class="entry-meta">
+												<li class="color">$29.99</li>
+												<li><i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star-half-full"></i></li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="spost clearfix">
+										<div class="entry-image">
+											<a href="#"><img src="images/shop/small/6.jpg" alt="Image"></a>
+										</div>
+										<div class="entry-c">
+											<div class="entry-title">
+												<h4><a href="#">Checked Short Dress</a></h4>
+											</div>
+											<ul class="entry-meta">
+												<li class="color">$23.99</li>
+												<li><i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star-half-full"></i> <i class="icon-star-empty"></i></li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="spost clearfix">
+										<div class="entry-image">
+											<a href="#"><img src="images/shop/small/7.jpg" alt="Image"></a>
+										</div>
+										<div class="entry-c">
+											<div class="entry-title">
+												<h4><a href="#">Light Blue Denim Dress</a></h4>
+											</div>
+											<ul class="entry-meta">
+												<li class="color">$19.99</li>
+												<li><i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star-empty"></i> <i class="icon-star-empty"></i></li>
+											</ul>
+										</div>
+									</div>
+
+								</div>
+
+							</div>
+
+							<div class="widget clearfix">
+
+								<h4>Last Viewed Items</h4>
+								<div class="widget-last-view">
+									<div class="spost clearfix">
+										<div class="entry-image">
+											<a href="#"><img src="images/shop/small/3.jpg" alt="Image"></a>
+										</div>
+										<div class="entry-c">
+											<div class="entry-title">
+												<h4><a href="#">Round-Neck Tshirt</a></h4>
+											</div>
+											<ul class="entry-meta">
+												<li class="color">$15</li>
+												<li><i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i></li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="spost clearfix">
+										<div class="entry-image">
+											<a href="#"><img src="images/shop/small/10.jpg" alt="Image"></a>
+										</div>
+										<div class="entry-c">
+											<div class="entry-title">
+												<h4><a href="#">Green Trousers</a></h4>
+											</div>
+											<ul class="entry-meta">
+												<li class="color">$19</li>
+												<li><i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star-empty"></i> <i class="icon-star-empty"></i> <i class="icon-star-empty"></i></li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="spost clearfix">
+										<div class="entry-image">
+											<a href="#"><img src="images/shop/small/11.jpg" alt="Image"></a>
+										</div>
+										<div class="entry-c">
+											<div class="entry-title">
+												<h4><a href="#">Silver Chrome Watch</a></h4>
+											</div>
+											<ul class="entry-meta">
+												<li class="color">$34.99</li>
+												<li><i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star-half-full"></i> <i class="icon-star-empty"></i></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+
+							</div>
+
+							<div class="widget clearfix">
+
+								<h4>Popular Items</h4>
+								<div id="Popular-item">
+									<div class="spost clearfix">
+										<div class="entry-image">
+											<a href="#"><img src="images/shop/small/8.jpg" alt="Image"></a>
+										</div>
+										<div class="entry-c">
+											<div class="entry-title">
+												<h4><a href="#">Pink Printed Dress</a></h4>
+											</div>
+											<ul class="entry-meta">
+												<li class="color">$21</li>
+												<li><i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star-half-full"></i></li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="spost clearfix">
+										<div class="entry-image">
+											<a href="#"><img src="images/shop/small/5.jpg" alt="Image"></a>
+										</div>
+										<div class="entry-c">
+											<div class="entry-title">
+												<h4><a href="#">Blue Round-Neck Tshirt</a></h4>
+											</div>
+											<ul class="entry-meta">
+												<li class="color">$19.99</li>
+												<li><i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star-empty"></i> <i class="icon-star-empty"></i></li>
+											</ul>
+										</div>
+									</div>
+
+									<div class="spost clearfix">
+										<div class="entry-image">
+											<a href="#"><img src="images/shop/small/12.jpg" alt="Image"></a>
+										</div>
+										<div class="entry-c">
+											<div class="entry-title">
+												<h4><a href="#">Men Aviator Sunglasses</a></h4>
+											</div>
+											<ul class="entry-meta">
+												<li class="color">$14.99</li>
+												<li><i class="icon-star3"></i> <i class="icon-star3"></i> <i class="icon-star-half-full"></i> <i class="icon-star-empty"></i> <i class="icon-star-empty"></i></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+
+							</div>
+
+							<div class="widget subscribe-widget clearfix">
+
+								<h4>Subscribe For Latest Offers</h4>
+								<h5>Subscribe to Our Newsletter to get Important News, Amazing Offers &amp; Inside Scoops:</h5>
+								<form action="#" role="form" class="notopmargin nobottommargin">
+									<div class="input-group divcenter">
+										<input type="text" class="form-control" placeholder="Enter your Email" required="">
+										<span class="input-group-btn">
+											<button class="btn btn-success" type="submit"><i class="icon-email2"></i></button>
+										</span>
+									</div>
+								</form>
+							</div>
+						</div>
